0560f1f1080a15250f5f63ec86b78263b8a116b0
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-27 16:02:48 +0100

    [enumerables]:Remove Unnecessary code

--- my_list.rb
+++ my_list.rb
@@ -17,14 +17,3 @@ class MyList
     @list.each { |value| yield value if block_given? }
   end
 end
-
-list = MyList.new(1, 2, 3, 4)
-
-puts list.all? { |e| e < 5 }
-
-puts list.all? { |e| e > 5 }
-
-puts list.any? { |e| e == 2 }
-puts list.any? { |e| e == 5 }
-
-puts list.filter { |e| e.even? }
