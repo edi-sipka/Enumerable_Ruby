c60e8b5e7a5d801dedf338202c5c222ffc54356f
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-27 15:52:06 +0100

    [enumerables]: Instantiate Class and test methods.

--- my_list.rb
+++ my_list.rb
@@ -1,18 +1,30 @@
-#Create a class MyList that has an instance variable @list.
+# Create a class MyList that has an instance variable @list.
+
 require_relative 'my_enumerable'
 
 class MyList
   def initialize(*list)
-    @list=list
+    @list = list
   end
 
+  # In MyList implement a method #each that yields successive members
 
+  # of @list and uses the MyEnumerable module.
 
-#In MyList implement a method #each that yields successive members 
-#of @list and uses the MyEnumerable module.
+  include MyEnumerable
 
-include MyEnumerable 
-def each
-  @list.each {|value| yield value if block_given?}
+  def each
+    @list.each { |value| yield value if block_given? }
+  end
 end
-end
+
+list = MyList.new(1, 2, 3, 4)
+
+puts list.all? { |e| e < 5 }
+
+puts list.all? { |e| e > 5 }
+
+puts list.any? { |e| e == 2 }
+puts list.any? { |e| e == 5 }
+
+puts list.filter { |e| e.even? }
